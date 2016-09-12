3da1e3fa18fcaf6043bce11ccb66d99c7ccaa84e
MD Alexson <md.alexson@gmail.com>
2016-09-12 16:31:57 +0200

    fix

--- resources/views/frontend/postsByCat.blade.php
+++ resources/views/frontend/postsByCat.blade.php
@@ -18,22 +18,48 @@
         <div class="row">
             @if($project->sidebar)
             <div class="col-md-8 thin">
-                @if(!empty($category->category_html))
-                <?php echo html_entity_decode($category->category_html); ?>
-                <br/>
+                @if(frontendCheckLevel($category, Session::get('level_id')))
+                        @if(!empty($category->category_html))
+                        <?php echo html_entity_decode($category->category_html); ?>
+                        <br/>
+                        @endif
+                        @include('frontend.inc.lists.posts_by_cat', ['posts' => $posts])
+                @elseif($category->upsale)
+                        <?php echo html_entity_decode($category->upsale_text); ?>
+                @elseif($category->posts()->where('upsale', true)->count())
+                        @if(!empty($category->category_html))
+                        <?php echo html_entity_decode($category->category_html); ?>
+                        <br/>
+                        @endif
+                        @include('frontend.inc.lists.posts_by_cat', ['posts' => $posts])
+                @else
+                <p style="color: #cc0000">Вы не имеете доступа к данной категории!</p>
+                <a href='/'>На главную</a>
                 @endif
-                @include('frontend.inc.lists.posts_by_cat', ['posts' => $posts])
             </div>
             <div class="col-md-4" id="sidebar">
                 <?php echo html_entity_decode($project->sidebar_html); ?>
             </div>
             @else
             <div class="col-md-10 col-md-offset-1 col-sm-12 wide">
-                @if(!empty($category->category_html))
-                <?php echo html_entity_decode($category->category_html); ?>
-                <br/>
+                @if(frontendCheckLevel($category, Session::get('level_id')))
+                        @if(!empty($category->category_html))
+                        <?php echo html_entity_decode($category->category_html); ?>
+                        <br/>
+                        @endif
+                        @include('frontend.inc.lists.posts_by_cat', ['posts' => $posts])
+                @elseif($category->upsale)
+                        <?php echo html_entity_decode($category->upsale_text); ?>
+                @elseif($category->posts()->where('upsale', true)->count())
+                        @if(!empty($category->category_html))
+                        <?php echo html_entity_decode($category->category_html); ?>
+                        <br/>
+                        @endif
+                        @include('frontend.inc.lists.posts_by_cat', ['posts' => $posts])
+                @else
+                <p style="color: #cc0000">Вы не имеете доступа к данной категории!</p>
+                <a href='/'>На главную</a>
                 @endif
-                @include('frontend.inc.lists.posts_by_cat', ['posts' => $posts])
             </div>
             @endif
         </div>
