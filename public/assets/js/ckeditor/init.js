afa5a33afae35064bde75eda04a19c49dadf55ac
MD Alexson <md.alexson@gmail.com>
2016-09-15 10:20:14 +0200

    cke style

--- public/assets/js/ckeditor/init.js
+++ public/assets/js/ckeditor/init.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $(".ckedit").ckeditor({
         language: 'ru',
         extraPlugins: 'undo,base64image,oembed,justify,colorbutton,font',
-        removePlugins: 'image'
+        removePlugins: 'image',
+        extraAllowedContent: 'style; *{*}'
     });
 });
