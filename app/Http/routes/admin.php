db033de53a0696fcaaa4f1913ceabd6a01b7c2dd
MD Alexson <md.alexson@gmail.com>
2016-12-03 10:49:54 +0100

    ipr

--- app/Http/routes/admin.php
+++ app/Http/routes/admin.php
@@ -159,11 +159,11 @@ support@abckabinet.ru\r
         
         $em1 = "chepkasov-john@mail.ru";
         $em2 = "khabibulin1981@mail.ru";
-        $pr1 = \App\Project::findOrFail("intensiv2016")->susers()->where('email', $em1);
+        $pr1 = \App\Project::findOrFail("intensiv2016")->susers()->where('email', $em1)->first();
         $pr1->ipr_key()->associate(getAvaibleKey());
         $pr1->save();
         
-        $pr2 = \App\Project::findOrFail("intensiv2016")->susers()->where('email', $em2);
+        $pr2 = \App\Project::findOrFail("intensiv2016")->susers()->where('email', $em2)->first();
         $pr2->ipr_key()->associate(getAvaibleKey());
         $pr2->save();
         
