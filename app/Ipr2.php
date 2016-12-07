5c135e6f99c42be9cdb9fef249d818113d48ab01
MD Alexson <md.alexson@gmail.com>
2016-12-07 02:45:53 +0100

    ipr2

---
+++ app/Ipr2.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ipr2 extends Model
+{
+    protected $table   = 'ipr2';
+    protected $primaryKey = 'key';
+    public $timestamps = false;
+    public $incrementing = false;
+}
