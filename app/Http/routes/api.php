927e05851ac5329d06d654a782251e171068ec36
MD Alexson <md.alexson@gmail.com>
2016-12-21 09:58:42 +0100

    fondy debug

--- app/Http/routes/api.php
+++ app/Http/routes/api.php
@@ -6,6 +6,13 @@ Route::group(['namespace' => 'Api', 'domain' => config('app.domain')],
     Route::post('/api/payment/eautopay', "PaymentController@eautopay");
     Route::post('/api/payment/justclick', "PaymentController@justclick");
     Route::any('/api/payment/fondy', "PaymentController@fondy");
+    Route::any('/api/payment/fondytest', function(){
+        $json_string = json_encode(\Request::all());
+        Mail::raw($json_string, function($message) {
+            $message->from('hostmaster@abckabinet.ru', 'ABC Кабинет');
+            $message->to("md.alexson@gmail.com")->subject('FONDY - POST - JSON');
+        });
+    });
     
     Route::get('/api/payment/success', "ViewController@success");
     Route::get('/api/payment/fail', "ViewController@fail");
